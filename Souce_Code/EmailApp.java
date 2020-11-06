89389617cd67e1021aeca90da78657611ba68663
Carl <71315504+carlx009@users.noreply.github.com>
2020-11-06 09:08:38 +0800

    Update EmailApp.java

--- Souce_Code/EmailApp.java
+++ Souce_Code/EmailApp.java
@@ -1,1 +1,12 @@
+package Emailapp;
 
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Email em1 = new Email("Carl","Yip");
+		System.out.println(em1.showInfo());
+		
+		
+	}
+}
